588720cb1ea4fb60492783d9095d23cb6b2cdb24
sherylsphilip <89590218+sherylsphilip@users.noreply.github.com>
2021-09-14 17:55:06 -0400

    Update VendingMachineServiceLayerImpl.java

--- src/main/java/service/VendingMachineServiceLayerImpl.java
+++ src/main/java/service/VendingMachineServiceLayerImpl.java
@@ -5,10 +5,66 @@
  */
 package service;
 
+import dao.VendingMachineAuditDao;
+import dao.VendingMachineDao;
+import dto.Product;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
 /**
  *
  * @author junha
  */
-public class VendingMachineServiceLayerImpl {
-    
+public class VendingMachineServiceLayerImplimplements VendingMachineServiceLayer {
+
+    private VendingMachineDao dao;
+    private VendingMachineAuditDao auditdao;
+
+    public VendingMachineServiceLayerImpl(VendingMachineDao dao, VendingMachineAuditDao auditdao) {
+        this.dao = dao;
+        this.auditdao = auditdao;
+    }
+
+    @Override
+    public List<Product> listItems() {//throws NoItemInventoryException;{
+        return dao.listProducts();
+    }
+
+    public void validateItemNumber() {
+        
+    }
+    /*
+    This method will check whether the user has sufficient fund
+    */
+   
+    public void validateSufficientFund(BigDecimal change, String productName) {
+        if(change.compareTo(BigDecimal.valueOf(0)) < 0){
+//            throw new InsufficientFundException(         
+//            
+//                    "ERROR: Could not buy the product "
+//                    + productName
+//                    + "...Insufficient Fund");
+        }
+        
+        
+    }
+
+    @Override
+    public void updateProduct(int productId) {
+        //dao.UpdateProduct(productId);
+        //auditdao.writeAuditentry();
+    }
+
+    /*
+    this method will retrieve the cost of the product and find the change
+     */
+    @Override
+    public BigDecimal calculateChanges(BigDecimal userMoney, int productID) {
+        Product product = dao.getProduct(productID);
+        BigDecimal change;
+        change = userMoney.subtract(product.getPrice());
+        validateSufficientFund(change, product.getProductName());
+        return change.setScale(2, RoundingMode.HALF_UP);
+    }
 }
