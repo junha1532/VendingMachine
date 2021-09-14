5d8bc8978c0a2625feae0128eb5b78726aaafe7d
AustinGraham28 <89654150+AustinGraham28@users.noreply.github.com>
2021-09-14 17:40:11 -0400

    Current view progress as of 9/14/2021 5:30p EST

--- src/main/java/ui/VendingMachineView.java
+++ src/main/java/ui/VendingMachineView.java
@@ -5,10 +5,47 @@
  */
 package ui;
 
+import dto.Product;
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  *
  * @author junha
  */
 public class VendingMachineView {
+    private UserIO io;
     
+    //constructor for call from the app file
+    public VendingMachineView(UserIO io){
+        this.io = io;
+    }
+    
+    public void displayProductList(List<Product> productList){
+        for(Product currentProduct: productList){
+            displayProductInfo(currentProduct);
+        }
+    }
+    
+    private void displayProductInfo(Product product){
+        io.print(product.getProductId() + " >> ");
+        io.print("\t" + product.getProductName());
+        io.print("\t" + product.getPrice());
+    }
+    
+    public int getProductChoice(){
+        int prodId = io.readInt("Please enter the I.D. number of you chosen item: ");
+        return prodId;
+    }
+    
+    /*
+    public Product addNewProduct(){
+        //need way to set ID
+        String name = io.readString("Enter product name: ");
+        BigDecimal price = new BigDecimal(io.readString("Enter prodcut price: "));
+        int numItems = io.readInt("Enter the total of this product being added to machine: ");
+        
+        return new Product(prodId, name, price, numItems);
+    }
+    */
 }
